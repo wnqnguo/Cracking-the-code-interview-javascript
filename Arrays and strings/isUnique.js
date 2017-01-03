ffa55d91e67c59dde2a0bbe969c5d72528a2d33e
Wenqian Faye Guo <Wenqian.Faye.Guo@macys.com>
2017-01-03 11:54:52 -0800

    isUnique

---
+++ Arrays and strings/isUnique.js
@@ -0,0 +1,14 @@
+function isUnique (str){
+  var temp = [];
+  if(str.length>26){
+    return false;
+  }
+  for(var i =0; i<str.length; i++){
+    if(temp[str.charCodeAt(i)]){
+      return false;
+    }else{
+      temp[str.charCodeAt(i)]=string[i];
+    }
+  }
+  return true;
+}
