0f3656ef1d40003110d0fc100fc59d667de1b92b
Wenqian Faye Guo <Wenqian.Faye.Guo@macys.com>
2017-01-19 13:30:23 -0800

    more

--- Chapter 8. Recursion and Dynamic Programming/8.7 Permutation Without Dups.js
+++ Chapter 8. Recursion and Dynamic Programming/8.7 Permutation Without Dups.js
@@ -1,3 +1,20 @@
-function permutation (){
+function permutation (str){
+  var result = [];
+  if(str.length == 1){
+    return str;
+  }
+  for(var i =0; i< str.length; i++){
+    var curr = str[i];
+    console.log("curr is "+curr);
+    var more = str.slice(0,i)+str.slice(i+1,str.length);
+    console.log("the rest is "+ more);
+    var temp = permutation(more);
+   for(var j =0; j<temp.length; j++){
+     result.push([curr+temp[j]]);
+   }
 
+  }
+  return result;
 }
+
+permutation('1234');
